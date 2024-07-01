55af9edf26886aa987e2403c684dc9adf065bba0
a-elhussein <a.elhussein61@gmail.com>
2024-07-01 19:09:49 +0300

    Image Upload

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+        // POST: /api/images/upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto imageUploadRequestDto) 
+        {
+            ValidateFileUpload(imageUploadRequestDto);
+
+            if(ModelState.IsValid) 
+            {
+                //Convert DTO to Domain Model
+                var imageDomainModel = new Image
+                {
+                    File = imageUploadRequestDto.File,
+                    FileExtension = Path.GetExtension(imageUploadRequestDto.File.FileName),
+                    FileSizeInBytes = imageUploadRequestDto.File.Length,
+                    FileName = imageUploadRequestDto.FileName,
+                    FileDescription = imageUploadRequestDto.FileDescription,
+                };
+                //User Repo to upload image
+                await imageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
+            }
+
+            return BadRequest(ModelState);
+
+        }
+        
+        private void ValidateFileUpload(ImageUploadRequestDto imageUploadRequestDto) 
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if(!allowedExtensions.Contains(Path.GetExtension(imageUploadRequestDto.File.FileName)))
+            {
+                ModelState.AddModelError("file", "UnsupportedFile");
+            }
+
+            if (imageUploadRequestDto.File.Length > 10485760) 
+            {
+                ModelState.AddModelError("file", "More than 10MB!");
+            }
+        }
+    }
+}
