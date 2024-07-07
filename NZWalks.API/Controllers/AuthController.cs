3a4e94427198020b2c292005d4682172b802fcc0
waleed1979 <w_ahdal@hotmail.com>
2024-07-07 09:16:55 +0300

    added comment

--- NZWalks.API/Controllers/AuthController.cs
+++ NZWalks.API/Controllers/AuthController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Models.DTO;
 using NZWalks.API.Repositories;
-
+// test
 namespace NZWalks.API.Controllers
 {
     [Route("api/[controller]")]
@@ -21,7 +21,7 @@ namespace NZWalks.API.Controllers
         // POST: /api/auth/Register
         [HttpPost]
         [Route("Register")]
-        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO) 
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
         {
             var identityUser = new IdentityUser
             {
@@ -31,18 +31,18 @@ namespace NZWalks.API.Controllers
 
             var identityResult = await userManager.CreateAsync(identityUser, registerRequestDTO.Password);
 
-            if (identityResult.Succeeded) 
+            if (identityResult.Succeeded)
             {
                 // Add role to this User
-                if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any()) 
+                if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any())
                 {
                     identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
 
-                    if (identityResult.Succeeded) 
+                    if (identityResult.Succeeded)
                     {
                         return Ok("User was registered!");
                     }
-                    
+
                 }
             }
 
@@ -55,18 +55,18 @@ namespace NZWalks.API.Controllers
         [HttpPost]
         [Route("Login")]
 
-        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequestDTO) 
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequestDTO)
         {
             var user  = await userManager.FindByEmailAsync(loginRequestDTO.Username);
-            if (user != null) 
+            if (user != null)
             {
                 var checkPasswordResult = await userManager.CheckPasswordAsync(user, loginRequestDTO.Password);
 
-                if (checkPasswordResult) 
+                if (checkPasswordResult)
                 {
                     //Get Roles for user
                     var roles = await userManager.GetRolesAsync(user);
-                    if (roles != null) 
+                    if (roles != null)
                     {
                         //Create Token
                         var jwtToken = tokenRepository.CreateJWTToken(user, roles.ToList());
@@ -77,15 +77,14 @@ namespace NZWalks.API.Controllers
 
                         return Ok(response);
                     }
-                    
+
                 }
             }
 
             return BadRequest("User or not found");
 
-            
+
         }
 
     }
 }
-
